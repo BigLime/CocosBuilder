593db7aa3f13dc1c967b330753154aaaedccaf65
Leem <songrong.li@zonjoy.net>
2017-04-10 21:31:41 +0800

    + 添加CCGLView

--- CocosBuilder/libs/cocos2d-bridge/CCGLView.h
+++ CocosBuilder/libs/cocos2d-bridge/CCGLView.h
@@ -8,67 +8,20 @@
 
 #import <Foundation/Foundation.h>
 
-@protocol CCEventDelegate <NSObject>
-// Mouse
-- (void)mouseDown:(NSEvent *)theEvent;
-- (void)mouseUp:(NSEvent *)theEvent;
-- (void)mouseMoved:(NSEvent *)theEvent;
-- (void)mouseDragged:(NSEvent *)theEvent;
-- (void)rightMouseDown:(NSEvent*)event;
-- (void)rightMouseDragged:(NSEvent*)event;
-- (void)rightMouseUp:(NSEvent*)event;
-- (void)otherMouseDown:(NSEvent*)event;
-- (void)otherMouseDragged:(NSEvent*)event;
-- (void)otherMouseUp:(NSEvent*)event;
-- (void)scrollWheel:(NSEvent *)theEvent;
-- (void)mouseEntered:(NSEvent *)theEvent;
-- (void)mouseExited:(NSEvent *)theEvent;
-
-
-// Keyboard
-- (void)keyDown:(NSEvent *)theEvent;
-- (void)keyUp:(NSEvent *)theEvent;
-- (void)flagsChanged:(NSEvent *)theEvent;
-
-// Touches
-- (void)touchesBeganWithEvent:(NSEvent *)event;
-- (void)touchesMovedWithEvent:(NSEvent *)event;
-- (void)touchesEndedWithEvent:(NSEvent *)event;
-- (void)touchesCancelledWithEvent:(NSEvent *)event;
-
-// Gestures
-- (void)beginGestureWithEvent:(NSEvent *)event;
-- (void)magnifyWithEvent:(NSEvent *)event;
-- (void)smartMagnifyWithEvent:(NSEvent *)event;
-- (void)rotateWithEvent:(NSEvent *)event;
-- (void)swipeWithEvent:(NSEvent *)event;
-- (void)endGestureWithEvent:(NSEvent *)event;
-
-@end
-
-/** CCGLView
- 
- Only available for Mac OS X
- */
-@interface CCGLView : NSOpenGLView {
-    id<CCEventDelegate> _eventDelegate;
+@interface CCGLView : NSObject {
+    void* impl_;
+    BOOL isNeedGLViewDealloc_;
 }
 
-/** Event delegate */
-@property (nonatomic, readwrite, assign) id<CCEventDelegate> eventDelegate;
+@property (nonatomic, readwrite) NSRect frame;
 
-/** initializes the CCGLView with a frame rect and an OpenGL context */
-- (id) initWithFrame:(NSRect)frameRect shareContext:(NSOpenGLContext*)context;
+- (NSPoint)convertPoint:(NSPoint)point toView:(nullable NSView*)view;
+- (void)addTrackingArea:(NSTrackingArea*)trackingArea;
+- (void)removeTrackingArea:(NSTrackingArea*)trackingArea;
 
-/** uses and locks the OpenGL context */
--(void) lockOpenGLContext;
+-(id)initWithFrame:(NSRect)frame;
+-(id)initWithObject: (void*)object;
 
-/** unlocks the openGL context */
--(void) unlockOpenGLContext;
+-(void*) getImpl;
 
-/** returns the depth format of the view in BPP */
-- (NSUInteger) depthFormat;
-
-// private
-+(void) load_;
 @end
